7a47a4a5c7f086f06738dbcaf31dd82447a7e55e
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-05-01 14:21:16 -0400

    :construction: Creating Dogs class

---
+++ src/collections/Dogs.java
@@ -0,0 +1,48 @@
+package collections;
+
+public class Dogs
+{
+    private String breed;
+    private int avgWeight;
+    private boolean apartment;
+
+    public Dogs(String breed, int avgWeight, boolean apartment)
+    {
+        this.breed=breed;
+        this.avgWeight=avgWeight;
+        this.apartment=apartment;
+    }
+
+    public String getBreed()
+    {
+        return breed;
+    }
+
+    public void setBreed(String breed)
+    {
+        this.breed = breed;
+    }
+    
+    public int getAvgWeight()
+    {
+        return avgWeight;
+    }
+
+    public void setAvgWeight(int avgWeight)
+    {
+        this.avgWeight = avgWeight;
+    }
+
+    // some people use is instead of get for boolean values
+
+    public boolean getApartment()
+    {
+        return apartment;
+    }
+
+    public void setApartment(boolean apartment)
+    {
+        this.apartment=apartment;
+    }
+
+}
